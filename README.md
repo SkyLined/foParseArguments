e0a3de2921a615346c943e9ba329195a72ee4655
SkyLined <berendjanwever@gmail.com>
2015-03-03 19:08:56 +0100

    Initial check-in

---
+++ README.md
@@ -0,0 +1,82 @@
+foParseArguments
+===============
+
+Parse Node.js arguments library.
+
+Getting Started
+---------------
+1. Install foParseArguments via NPM.
+  
+  `npm install foparsearguments`
+  
+  Optionally: rename `foparsearguments` to `foParseArguments`: npm is unable to
+  handle the complexity of uppercase characters in a module name. Node on
+  Windows does not have this problem, so renaming the folder is not required
+  for you to use the module.
+  
+2. Require foParseArguments in your project.
+  
+  `var foParseArguments=require("foParseArguments");`
+
+3. Parse arguments using foParseArguments.
+  
+  ```
+  var oArguments = foParseArguments({
+    "dfxTypesConverters": { 
+      "myType": function (sValue) {
+        if (!isvalid(sValue) throw new Error("must be a valid xxx");
+        return processed(sValue);
+      }, // define a type
+      ...
+    },
+    "adxParameters": [ 
+      {
+        "sName": "first parameter",
+        "sTypeDescription": "string",
+        "sHelpText": "This will be shown when you run the script with -?",
+        "xDefaultValue": "first"
+      },
+      {
+        "sName": "second parameter",
+        "sTypeDescription": "int",
+        "sHelpText": "This will be shown when you run the script with -?",
+        "xDefaultValue": 2
+      },
+      ...
+    ],
+    "dxSwitches": {
+      "switch-name": {
+        "sHelpText": "This will be shown when you run the script with -?",
+      },
+      ...
+    ],
+    "dxOptions": {
+      "option-name": {
+        "sTypeDescription": "myType", // option must be of mytype.
+        "sHelpText": "This will be shown when you run the script with -?",
+        "xDefaultValue": 2
+      },
+      ...
+    }
+  });
+  ```
+  
+4. Used parsed arguments in your script
+  
+  ```  
+  var uSomeNumber = oArguments.dxOptions["some-number"];
+  var sSomeString = oArguments.axParameters[0];
+  var bSomeBoolean = oArguments.dbSwitches["some-boolean"];
+  ```
+  
+Notes
+-----
+### Alpha release
+
+This has not been extensively tested and the API will most likely change in the
+future. 
+
+--------------------------------------------------------------------------------
+
+### License
+This code is licensed under [CC0 v1.0 Universal](https://creativecommons.org/publicdomain/zero/1.0/).
---
+++ foParseArguments.js
@@ -0,0 +1,287 @@
+var oPath = require("path");
+function fShowHelp(oSettings) {
+  asUsage = [oPath.basename(process.argv[1])];
+  if (oSettings.adxParameters) {
+    for (var u = 0; u < oSettings.adxParameters.length; u++) {
+      var oParameterSettings = oSettings.adxParameters[u],
+          sParameterName = oParameterSettings.sName,
+          auParameterRepeat = oParameterSettings.auRepeat;
+      if (!oParameterSettings.auRepeat) {
+        asUsage.push(sParameterName);
+      } else {
+        for (var uRepeat = 0; uRepeat < oParameterSettings.auRepeat[1]; uRepeat++) {
+          if (uRepeat < oParameterSettings.auRepeat[0]) {
+            asUsage.push(sParameterName);
+          } else if (oParameterSettings.auRepeat[1] == Infinity) {
+            asUsage.push("[...]");
+          } else {
+            asUsage.push("[" + sParameterName + "]");
+          }
+        }
+      }
+    }
+  }
+  if (oSettings.dxOptions) {
+    if (oSettings.dxSwitches) {
+      asUsage.push("[options and switches]");
+    } else {
+      asUsage.push("[options]");
+    }
+  } else if (oSettings.dxSwitches) {
+    asUsage.push("[switches]");
+  }
+  console.log("Usage:");
+  console.log("    " + asUsage.join(" "));
+  console.log("");
+  if (oSettings.adxParameters) {
+    console.log("Parameters:");
+    for (var u = 0; u < oSettings.adxParameters.length; u++) {
+      var oParameterSettings = oSettings.adxParameters[u];
+      console.log("    " + sParameterName + " (" + oParameterSettings.sTypeDescription + ")");
+      console.log("        " + oParameterSettings.sHelpText);
+      if (oParameterSettings.xDefaultValue !== undefined) {
+        console.log("        default: " + JSON.stringify(oParameterSettings.xDefaultValue));
+      }
+    }
+  }
+  if (oSettings.dxOptions) {
+    console.log("Options:");
+    for (var sOptionName in oSettings.dxOptions) {
+      var oOptionSettings = oSettings.dxOptions[sOptionName],
+          sAdditionalName = oOptionSettings.sShortName ? " (or -" + oOptionSettings.sShortName + "=...)" : "";
+      console.log("    --" + sOptionName + "=" + oOptionSettings.sTypeDescription + sAdditionalName);
+      console.log("        " + oOptionSettings.sHelpText);
+      if (oOptionSettings.xDefaultValue !== undefined) {
+        console.log("        default: " + JSON.stringify(oOptionSettings.xDefaultValue));
+      }
+    }
+  }
+  if (oSettings.dxSwitches) {
+    console.log("Switches:");
+    for (var sSwitchName in oSettings.dxSwitches) {
+      console.log("    --" + sSwitchName);
+      console.log("        " + oSettings.dxSwitches[sSwitchName].sHelpText);
+    }
+  }
+}
+function fShowError(sMessage) {
+  console.log("Invalid argument syntax: " + sMessage);
+}
+function fxParseValueUsingTypeDescription(sName, sValue, sTypeDescription, dfxTypeConverters) {
+  asSubTypesAndSeparators = sTypeDescription.split(/\b/);
+  if (asSubTypesAndSeparators.length == 1) {
+    if (sTypeDescription in dfxTypeConverters) {
+      var fxTypeConverter = dfxTypeConverters[sTypeDescription];
+      try {
+        return fxTypeConverter(sValue);
+      } catch (oError) {
+        return fShowError(sName + " " + oError.message);
+      }
+    } else {
+      throw new Error("Unknown type \"" + sTypeDescription + "\" for " + sName);
+    }
+  }
+  var axValue = [];
+  for (var u = 0; u < asSubTypesAndSeparators.length; u += 2) {
+    var sSubName = sName + ", value #" + (u / 2 + 1),
+        sSubTypeDescriptor = asSubTypesAndSeparators[u],
+        sSeparator = asSubTypesAndSeparators[u + 1];
+    if (sSubTypeDescriptor in dfxTypeConverters) {
+      var fxTypeConverter = dfxTypeConverters[sSubTypeDescriptor];
+      if (!sSeparator) {
+        try {
+          var xValue = fxTypeConverter(sValue);
+        } catch (oError) {
+          return fShowError(sSubName + " " + oError.message);
+        }
+      } else {
+        var asValue = sValue.split(sSeparator);
+        if (asValue.length == 1) {
+          return fShowError(sSubName + " is not followed by a \"" + sSeparator + "\" separator");
+        }
+        try {
+          xValue = fxTypeConverter(asValue.shift());
+        } catch (oError) {
+          return fShowError(sSubName + " " + oError.message);
+        }
+        sValue = asValue.join(sSeparator);
+      }
+      axValue.push(xValue);
+    } else {
+      throw new Error("Unknown type \"" + sSubTypeDescriptor + "\" for " + sSubName);
+    }
+  }
+  return axValue;
+}
+
+function foParseArguments(oSettings, asArguments) {
+  var asArguments = asArguments || process.argv.slice(2);
+  var dfxTypeConverters = {
+    "string": function (sName, sValue) {
+      return sValue;
+    },
+    "bool": function (sValue) {
+      switch (sValue.toLowerCase()) {
+        case "false": case "0": case "off": case "no": case "disabled":
+          return false;
+        case "true": case "1": case "on": case "yes": case "enabled":
+          return true;
+      }
+      throw new Error("must be boolean");
+    },
+    "int": function (sValue) {
+      var iValue = parseInt(sValue);
+      if (isNaN(iValue)) throw new Error("must be an integer");
+      return iValue;
+    },
+    "uint": function (sValue) {
+      var uValue = parseInt(sValue);
+      if (isNaN(uValue) || uValue < 0) throw new Error("must be a positive integer");
+      return uValue;
+    },
+    "float": function (sValue) {
+      var nValue = parseFloat(sValue);
+      if (isNaN(nValue)) throw new Error("must be a number");
+      return nValue;
+    },
+    "ufloat": function (sValue) {
+      var nValue = parseFloat(sValue);
+      if (isNaN(nValue) || nValue < 0) throw new Error("must be a positive number");
+      return nValue;
+    },
+  };
+  if ("dfxTypesConverters" in oSettings) {
+    for (sTypeName in oSettings.dfxTypesConverters) {
+      dfxTypeConverters[sTypeName] = oSettings.dfxTypesConverters[sTypeName];
+    }
+  }
+  var dsOptionTypeDescriptions = {},
+      dsOptionNames = {},
+      dxOptions = {};
+  if (oSettings.dxOptions) {
+    for (sOptionName in oSettings.dxOptions) {
+      var oOptionSettings = oSettings.dxOptions[sOptionName];
+      dsOptionTypeDescriptions[sOptionName] = oOptionSettings.sTypeDescription;
+      dsOptionNames[sOptionName] = sOptionName;
+      if (oOptionSettings.xDefaultValue !== undefined) {
+        dxOptions[sOptionName] = oOptionSettings.xDefaultValue;
+      }
+      if (oOptionSettings.sShortName) {
+        if (oOptionSettings.sShortName in dsOptionNames) {
+          throw new Error("short name " + oOptionSettings.sShortName + " is used more than once");
+        }
+        dsOptionNames[sShortName] = oOptionSettings.sShortName;
+      }
+    }
+  }
+  var dxSwitchNames = {},
+      dbSwitches = {};
+  if (oSettings.dxSwitches) {
+    for (var sSwitchName in oSettings.dxSwitches) {
+      dxSwitchNames[sSwitchName] = 1;
+      dbSwitches[sSwitchName] = false;
+    }
+  }
+  var asParameterNames = [],
+      asParameterTypeDescriptions = [],
+      aauParameterRepeats = [],
+      dxParameters = {};
+  if (oSettings.adxParameters) {
+    for (var u = 0; u < oSettings.adxParameters.length; u++) {
+      var oParameterSettings = oSettings.adxParameters[u];
+      if (oParameterSettings.sName in dxParameters) {
+        throw new Error("Parameter name \"" + oParameterSettings.sName + "\" is used more than once");
+      }
+      asParameterNames.push(oParameterSettings.sName);
+      asParameterTypeDescriptions.push(oParameterSettings.sTypeDescription);
+      aauParameterRepeats.push(oParameterSettings.auRepeat);
+      dxParameters[oParameterSettings.sName] = undefined;
+    }
+  }
+  var bNoMoreOptionsOrSwitches = false,
+      uParameterIndex = 0,
+      uParameterRepeat = 0;
+  for (var u = 2; u < process.argv.length; u++) {
+    var sArgument = process.argv[u];
+    if (sArgument == "--") {
+      bNoMoreOptionsOrSwitches = true;
+      continue;
+    }
+    var oOptionOrSwitchMatch = (
+        bNoMoreOptionsOrSwitches ? false :
+        sArgument.match(/^\-(?:([^:=])|\-([^:=]+))(?:[:=](.+))?$/)
+    );
+    if (oOptionOrSwitchMatch) {
+      if (sArgument == "-?" || sArgument == "--help") {
+        return fShowHelp(oSettings);
+      }
+      var sName = oOptionOrSwitchMatch[1] || oOptionOrSwitchMatch[2],
+          sValue = oOptionOrSwitchMatch[3];;
+      if (sName in dxSwitchNames) {
+        if (sValue) {
+          return fShowError("Switch \"" + sName + "\" does not take a value");
+        }
+        dbSwitches[sName] = true;
+      } else if (sName in dsOptionNames) {
+        var sOptionName = dsOptionNames[sName]; // sName might be a short name
+        if (!sValue) {
+          if (++u < process.argv.length) {
+            sValue = process.argv[u];
+          } else {
+            return fShowError("Missing value for \"" + sName + "\"");
+          }
+        }
+        var xOptionValue = fxParseValueUsingTypeDescription("option " + sOptionName, sValue, 
+            dsOptionTypeDescriptions[sOptionName], dfxTypeConverters);
+        if (xOptionValue === undefined) {
+          return;
+        }
+        dxOptions[sOptionName] = xOptionValue;
+      } else {
+        return fShowError("Unknown options \"" + sName + "\"");
+      }
+    } else {
+      var sParameterName = asParameterNames[uParameterIndex],
+          sParameterTypeDescription = asParameterTypeDescriptions[uParameterIndex],
+          asParameterRepeats = aasParameterRepeats[uParameterIndex];
+      if (asParameterRepeats) {
+        if (++uParameterRepeat == asParameterRepeats[1]) {
+          uParameterIndex++;
+          uParameterRepeat = 0;
+        }
+        var sValueName = "parameter " + sParameterName + ", value #" + uParameterRepeat;
+        var xValue = fxParseValueUsingTypeDescription(
+            sValueName, sArgument, sParameterTypeDescription, dfxTypeConverters);
+        if (dxParameters[sParameterName] === undefined) {
+          dxParameters[sParameterName] = [xValue];
+        } else {
+          dxParameters[sParameterName].push(xValue);
+        }
+      } else {
+        var sValueName = "parameter " + sParameterName;
+        xValue = fxParseValueUsingTypeDescription(
+            sValueName, sArgument, sParameterTypeDescription, dfxTypeConverters);
+        dxParameters[sParameterName] = xValue;
+        uParameterIndex++;
+        uParameterRepeat = 0;
+      }
+      axParameters.push(xParameterValue);
+    }
+  }
+  while (uParameterIndex < asParameterNames.length) {
+    if (!aasParameterRepeats[uParameterIndex] || uParameterRepeat == 0) {
+      return fShowError("Missing parameter " + asParameterNames[uParameterIndex]);
+    } else if (uParameterRepeat < aasParameterRepeats[uParameterIndex][0]) {
+      return fShowError("Missing parameter " + asParameterNames[uParameterIndex] + ", value #" + (uParameterRepeat + 1));
+    }
+    uParameterIndex++;
+    uParameterRepeat = 0;
+  }
+  return {
+    "dxParameters": dxParameters,
+    "dbSwitches": dbSwitches,
+    "dxOptions": dxOptions,
+  }
+}
+
+module.exports = foParseArguments;
